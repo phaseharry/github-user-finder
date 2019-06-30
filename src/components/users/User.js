1ece419bd012ea1285ebb5fb741a4ef3b17977fd
Harry C <harrychenhc@icloud.com>
2019-06-30 11:20:20 -0400

    worked on the detailed user profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
 import Spinner from '../layout/Spinner'
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login) //grabs the login parameter from our url we stated in the Route in the App component
   }
+
+  static propTypes = {
+    users: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired
+  }
+
   render() {
     const {
       name,
@@ -14,6 +24,7 @@ class User extends Component {
       blog,
       login,
       html_url,
+      company,
       followers,
       following,
       public_repos,
@@ -23,7 +34,71 @@ class User extends Component {
     if (this.props.loading) {
       return <Spinner />
     }
-    return <div>{name}</div>
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt="Github user profile image"
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location ? location : 'N/A'}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Blog: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
+      </Fragment>
+    )
   }
 }
 

909e6b2ab9d9854d5787c3f3a25dad66ffd0a0cf
Harry Chen <harrychenhc@icloud.com>
2019-06-30 20:01:51 -0400

    refactored Search and User component to use Hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,60 +1,57 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-  state = {
-    text: ''
-  }
+const Search = props => {
+  const { showClear, clearUsers, searchUsers, setAlert } = props
+  const [text, setText] = useState('')
+  /*
+    useState takes an initial state value as the argument and returns an array. 
+    The first element of the array is the actual state value and the second element 
+    is an function used to update that state.
+  */
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    setAlert: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
-  }
+  /* 
+    to give functional components something like methods, 
+    we can simply add functions within the functional component
+  */
+  const onChange = event => setText(event.target.value)
 
-  onChange = event => this.setState({ [event.target.name]: event.target.value })
-
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault() //need to prevent the default behavior for submit (it reloads the page)
-    if (!this.state.text) {
-      this.props.setAlert('Please enter something', 'light')
+    if (!text) {
+      setAlert('Please enter something', 'light')
     } else {
-      this.props.searchUsers(this.state.text)
-      this.setState({
-        text: ''
-      })
+      searchUsers(text)
+      setText('')
     }
   }
 
-  render() {
-    const { onChange, onSubmit } = this
-    const { showClear, clearUsers } = this.props
-    const { text } = this.state
-    return (
-      <div>
-        <form className="form" onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            onChange={onChange}
-            value={text}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form className='form' onSubmit={onSubmit}>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          onChange={onChange}
+          value={text}
+        />
+        <input type='submit' value='Search' className='btn btn-dark btn-block' />
+      </form>
+      {showClear && (
+        <button className='btn btn-light btn-block' onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  )
+}
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  setAlert: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired
 }
 
 export default Search
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,113 +1,116 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
 
-class User extends Component {
-  componentDidMount() {
-    const { getUser, getUserRepos, match } = this.props
+const User = props => {
+  const { getUser, getUserRepos, match, loading, repos } = props
+
+  useEffect(() => {
     getUser(match.params.login) //grabs the login parameter from our url we stated in the Route in the App component
     getUserRepos(match.params.login)
+    // eslint-disable-next-line
+  }, [])
+  /* useEffect hooks runs the effect(callback function passed in) after every render.
+   This would lead to an infinite loop if you want to make a data fetch and store it in state (componentDidMount). 
+   To stop it, we pass in an empty array as the 2nd argument to useEffect
+  */
+
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    company,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = props.user
+
+  if (loading) {
+    return <Spinner />
   }
-
-  static propTypes = {
-    users: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    loading: PropTypes.bool.isRequired
-  }
-
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      company,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user
-    const { loading, repos } = this.props
-
-    if (loading) {
-      return <Spinner />
-    }
-    return (
-      <Fragment>
-        <Link to="/" className="btn btn-light">
-          Back to Search
-        </Link>
-        Hireable:{' '}
-        {hireable ? (
-          <i className="fas fa-check text-success" />
-        ) : (
-          <i className="fas fa-times-circle text-danger" />
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              className="round-img"
-              alt="Github user profile"
-              style={{ width: '150px' }}
-            />
-            <h1>{name}</h1>
-            <p>Location: {location ? location : 'N/A'}</p>
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit Github Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Username: </strong> {login}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <Fragment>
-                    <strong>Company: </strong> {company}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <Fragment>
-                    <strong>Website: </strong> {blog}
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+  return (
+    <Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back to Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
+        <i className='fas fa-times-circle text-danger' />
+      )}
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            alt='Github user profile'
+            style={{ width: '150px' }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location ? location : 'N/A'}</p>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-light">Public Repos: {public_repos}</div>
-          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </Fragment>
-    )
-  }
+      </div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  )
+}
+
+User.propTypes = {
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 export default User
